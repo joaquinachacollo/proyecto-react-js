2f18405fb528a10490bbbcd79aefb7bc0e572c14
joaquin achacollo <joaquinachacollo@gmail.com>
2022-11-03 18:30:23 -0300

    preEntrega1-achacollo

---
+++ src/CartWidget.js
@@ -0,0 +1,6 @@
+import Logo from "./img/logo.jpg"
+const CartWidget = () => {
+    <img src={Logo} alt=""></img>
+}
+
+export default CartWidget
---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import NavBar from "./styles/Navbar.css"
+import CartWidget from "./CartWidget";
+const Navbar = () => {
+    return(
+    <header>
+        <div className="logo">
+            <img src={CartWidget} alt=""></img>
+        </div>
+        <nav>
+            <ul className="nav">
+                <li><a href="">Inicio</a></li>
+                <li><a href="">Productos</a></li>
+                <li><a href="">Contactos</a></li>
+            </ul>
+        </nav>
+    </header>
+    );
+}
+
+export default Navbar
